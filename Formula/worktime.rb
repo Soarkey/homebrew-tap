4dac85757b8f6df52d56c0bdfe589b40f5ea9bec
kirin <soarkey@foxmail.com>
2026-05-29 21:01:23 +0800

    Update worktime.rb

--- Formula/worktime.rb
+++ Formula/worktime.rb
@@ -1,22 +1,14 @@
 class Worktime < Formula
   desc "macOS 上下班时间监测菜单栏工具，通过 pmset 日志识别上下班时间"
   homepage "https://github.com/Soarkey/worktime"
-  url "https://github.com/Soarkey/worktime/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "f26b6b4f82c6a3a65023f305eb0d5f5e7748bcffda24a25efe53f16e21d13476"
+  url "https://github.com/Soarkey/worktime/releases/download/v0.1.2/worktime_0.1.2_darwin_arm64.tar.gz"
+  sha256 "031cc56adafa2646e0ee39538103710b3e89ffd4537894dcfa55299bb624a5e8"
   license "MIT"
-  head "https://github.com/Soarkey/worktime.git", branch: "main"
 
-  depends_on "go" => :build
   depends_on :macos
 
   def install
-    ENV["GOPROXY"] = "https://goproxy.cn,direct"
-    ldflags = "-s -w -X main.version=#{version}"
-    system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/worktime"
-  end
-
-  def post_install
-    system bin/"worktime", "--help" 
+    bin.install "worktime"
   end
 
   test do
