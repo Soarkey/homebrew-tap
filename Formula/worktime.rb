d375b5ae24f198499ba8885b6af8df91f9fbac7a
kirin <xiangwen@shu.edu.cn>
2026-05-29 15:59:00 +0800

    Update worktime.rb

--- Formula/worktime.rb
+++ Formula/worktime.rb
@@ -1,8 +1,8 @@
 class Worktime < Formula
   desc "macOS 上下班时间监测菜单栏工具，通过 pmset 日志识别上下班时间"
   homepage "https://github.com/Soarkey/worktime"
-  url "https://github.com/Soarkey/worktime/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "3cc99b128b997eae06b64462c5193b54d04236b81f6bd7adeeabdfc8c1566e41"
+  url "https://github.com/Soarkey/worktime/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "dccee8eca8707eb88c960920b9d218da630710e687f0945a45c36dfef389c349"
   license "MIT"
   head "https://github.com/Soarkey/worktime.git", branch: "main"
 
