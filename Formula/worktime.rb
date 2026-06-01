6ffb8e6fce028089d9809d83c2fd4fd20b4ea90e
soarkey <soarkey@foxmail.com>
2026-06-01 13:25:57 +0800

    Update worktime.rb

--- Formula/worktime.rb
+++ Formula/worktime.rb
@@ -2,10 +2,9 @@ class Worktime < Formula
   desc "macOS 上下班时间监测菜单栏工具，通过 pmset 日志识别上下班时间"
   homepage "https://github.com/Soarkey/worktime"
   url "https://github.com/Soarkey/worktime/releases/download/v0.1.4/worktime_0.1.4_darwin_arm64.tar.gz"
+  version "0.1.4"
   sha256 "4da8d00c814958779f89e34bbb5c1a766f45b4ff257fec27bd9395befa85f77e"
   license "MIT"
-
-  depends_on :macos
 
   def install
     bin.install "worktime"
